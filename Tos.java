6af694c6e4086422e0a239e367989eb0fa648ea2
s1290040 <s1290040@u-aizu.ac.jp>
2022-07-15 12:23:21 +0900

    User name added

--- Tos.java
+++ Tos.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 import java.util.Random;
 class Tos {
     public static void main(String[] args){
         int i,a=0,b=0;
         Random ran =new Random();
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Who are you?");
+	String name= scan.nextLine();
+	System.out.println("Hello, "+name+"!");
+	
 
-        System.out.println("Tossing acoin...");
+        System.out.println("Tossing a coin...");
         for(i=1;i<4;i++){
             int num=ran.nextInt(2);
             if(num==1){
